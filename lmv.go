ca72ff5487e532a81a6f6da76b8a75092442b667
Mihir Singh <me@mihirsingh.com>
2014-03-21 01:15:58 -0400

    Use MessagePack for .lmv files

--- lmv.go
+++ lmv.go
@@ -1,52 +1,53 @@
 package main
 
-import	(
-	"os"
-	"fmt"
-	"crypto/sha512"
-	"encoding/hex"
-	"hash"
-	"path/filepath" 
-	"flag"
-	"net/http" 
-	"net/url"
-	"io/ioutil" 	
-	"log"
-	"strconv"
+import  (
+    "os"
+    "fmt"
+    "crypto/sha512"
+    "encoding/hex"
+    "github.com/hinasssan/msgpack-go"
+    "hash"
+    "path/filepath" 
+    "flag"
+    "net/http" 
+    "net/url"
+    "io/ioutil"     
+    "log"
+    "strconv"
 )
 
 type LMVFile struct {
-	hash string
-	size int64
-	name string
+    Hash string `msgpack:"hash"`
+    Size int64  `msgpack:"size"`
+    Name string `msgpack:"name"`
 }
 
 func CalculateSHA512(data []byte) string {
 
-	var hasher hash.Hash = sha512.New()
+    var hasher hash.Hash = sha512.New()
 
-	hasher.Reset()
-	hasher.Write(data)
-	return hex.EncodeToString(hasher.Sum(nil))
+    hasher.Reset()
+    hasher.Write(data)
+    return hex.EncodeToString(hasher.Sum(nil))
 
 }
 
 func encode(fp string, token bool) {
 
-	lmv_file := new(LMVFile)
+    lmv_file := new(LMVFile)
 
-	file, err := os.Open(fp)
+    file, err := os.Open(fp)
 
     if err != nil {
-    	log.Fatal(err)
+        log.Fatal(err)
     }
 
-    defer file.Close()	
+    defer file.Close()  
 
     stat, err := file.Stat()
 
     if err != nil {
-    	log.Fatal(err)
+        log.Fatal(err)
     }
 
     bs := make([]byte, stat.Size())
@@ -56,46 +57,50 @@ func encode(fp string, token bool) {
         log.Fatal(err)
     }
 
-    lmv_file.hash = CalculateSHA512(bs)
-    lmv_file.size = stat.Size()
-    lmv_file.name = filepath.Base(fp)
+    lmv_file.Hash = CalculateSHA512(bs)
+    lmv_file.Size = stat.Size()
+    lmv_file.Name = filepath.Base(fp)
 
-	if token {
+    if token {
 
-    	upload_address := "http://127.0.0.1:8081/upload"
-		
-		fields := make(url.Values)
-        fields.Set("name", lmv_file.name)
-        fields.Set("hash", lmv_file.hash)
-        fields.Set("size", strconv.FormatInt(lmv_file.size, 10))
-    	
-    	resp, err := http.PostForm(upload_address, fields)
+        upload_address := "http://127.0.0.1:8081/upload"
+        
+        fields := make(url.Values)
+        fields.Set("name", lmv_file.Name)
+        fields.Set("hash", lmv_file.Hash)
+        fields.Set("size", strconv.FormatInt(lmv_file.Size, 10))
+        
+        resp, err := http.PostForm(upload_address, fields)
 
-    	if err != nil {
-			log.Fatal(err)
-		}
-		
-		defer resp.Body.Close()
+        if err != nil {
+            log.Fatal(err)
+        }
+        
+        defer resp.Body.Close()
 
-		body, err := ioutil.ReadAll(resp.Body)
+        body, err := ioutil.ReadAll(resp.Body)
 
-		if err != nil {
-			log.Fatal(err)
-		}
+        if err != nil {
+            log.Fatal(err)
+        }
 
-		fmt.Println("'" + lmv_file.name + "'" + " --> " + "'" + string(body) + "'")
-	
-	} else {
+        fmt.Println("'" + lmv_file.Name + "'" + " --> " + "'" + string(body) + "'")
+    
+    } else {
 
-		os.Create(lmv_file.name + ".lmv")
+        os.Create(lmv_file.Name + ".lmv")
 
-		full := lmv_file.hash + "\n" + lmv_file.name + "\n" + strconv.FormatInt(lmv_file.size, 10)
+        b, err := msgpack.Marshal(lmv_file)
 
-		err = ioutil.WriteFile(lmv_file.name + ".lmv", []byte(full), 0644)
-    	
-		if err != nil { 
-			log.Fatal(err)
-		}
+        if err != nil {
+            log.Fatal(err)
+        }    
+
+        err = ioutil.WriteFile(lmv_file.Name + ".lmv", b, 0644)
+        
+        if err != nil { 
+            log.Fatal(err)
+        }
 
     }
 
@@ -103,25 +108,25 @@ func encode(fp string, token bool) {
 
 func main() {
 
-	token := flag.Bool("token", false, "Use tokens in place of .lmv files")
+    token := flag.Bool("token", false, "Use tokens in place of .lmv files")
 
-	flag.Parse()
+    flag.Parse()
 
-	if len(os.Args) < 2 {
+    if len(os.Args) < 2 {
 
-		fmt.Println("Use lmv -h for usage")
+        fmt.Println("Use lmv -h for usage")
 
-	} else {
+    } else {
 
-		for i := 0; i < len(os.Args[1:]); i++ {
-	     
-			if _, err := os.Stat(os.Args[i+1]); err == nil {
+        for i := 0; i < len(os.Args[1:]); i++ {
+         
+            if _, err := os.Stat(os.Args[i+1]); err == nil {
 
-	    		encode(os.Args[i+1], *token)
+                encode(os.Args[i+1], *token)
 
-			}
+            }
 
-	    }
+        }
 
-	}
+    }
 }
