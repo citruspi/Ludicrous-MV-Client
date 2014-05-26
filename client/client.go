ea951b3ee5f860133cdaf4087f91ca90db1af626
Mihir Singh <me@mihirsingh.com>
2014-05-26 19:12:21 -0400

    "Properly" format code with go fmt

--- client/client.go
+++ client/client.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-    "archive/tar"
-    "bytes"
-    "crypto/sha512"
-    "encoding/hex"
-    "encoding/json"
-    "flag"
-    "fmt"
-    "hash"
-    "io/ioutil"
-    "net/http"
-    "net/url"
-    "os"
-    "path/filepath"
-    "strings"
-    "time"
+	"archive/tar"
+	"bytes"
+	"crypto/sha512"
+	"encoding/hex"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"hash"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 
-    "github.com/hinasssan/msgpack-go"
-    "github.com/Sirupsen/logrus"
+	"github.com/Sirupsen/logrus"
+	"github.com/hinasssan/msgpack-go"
 )
 
 // CONSTANTS
@@ -30,498 +30,498 @@ var log = logrus.New()
 var v bool = false
 
 type LMVFile struct {
-    Size int64  `msgpack:"size"`
-    Name string `msgpack:"name"`
-    Algorithm string `msgpack:"algorithm"`
-    Chunks []LMVChunk `msgpack:"chunks"`
-    Tar bool `msgpack:"tar"`
+	Size      int64      `msgpack:"size"`
+	Name      string     `msgpack:"name"`
+	Algorithm string     `msgpack:"algorithm"`
+	Chunks    []LMVChunk `msgpack:"chunks"`
+	Tar       bool       `msgpack:"tar"`
 }
 
 type LMVChunk struct {
-    Hash string `msgpack:"hash"`
-    Size int64 `msgpack:"size"`
-    Index int `msgpack:"index"`
+	Hash  string `msgpack:"hash"`
+	Size  int64  `msgpack:"size"`
+	Index int    `msgpack:"index"`
 }
 
 func CalculateSHA512(data []byte) string {
 
-    var hasher hash.Hash = sha512.New()
+	var hasher hash.Hash = sha512.New()
 
-    hasher.Reset()
-    hasher.Write(data)
-    return hex.EncodeToString(hasher.Sum(nil))
+	hasher.Reset()
+	hasher.Write(data)
+	return hex.EncodeToString(hasher.Sum(nil))
 
 }
 
 func TarballDirectory(fp string) string {
 
-    f, err := ioutil.TempFile("", "")
+	f, err := ioutil.TempFile("", "")
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    type WalkedFile struct {
-        Path string
-        Info os.FileInfo
-    }
+	type WalkedFile struct {
+		Path string
+		Info os.FileInfo
+	}
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    defer f.Close()
+	defer f.Close()
 
-    tw := tar.NewWriter(f)
+	tw := tar.NewWriter(f)
 
-    files := make([]WalkedFile, 0)
+	files := make([]WalkedFile, 0)
 
-    tarball := func(path string, info os.FileInfo, err error) error {
+	tarball := func(path string, info os.FileInfo, err error) error {
 
-        if info.IsDir() {
-            //
-        } else {
+		if info.IsDir() {
+			//
+		} else {
 
-            files = append(files, WalkedFile{
-                Path: path,
-                Info: info,
-            })
+			files = append(files, WalkedFile{
+				Path: path,
+				Info: info,
+			})
 
-        }
-        return nil
+		}
+		return nil
 
-    }
+	}
 
-    err = filepath.Walk(fp, tarball)
-    if err != nil {
-        log.Fatal(err)
-    }
+	err = filepath.Walk(fp, tarball)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    for _, fr := range files {
+	for _, fr := range files {
 
-        hdr := &tar.Header{
-            Name: fr.Path,
-            Size: fr.Info.Size(),
-        }
+		hdr := &tar.Header{
+			Name: fr.Path,
+			Size: fr.Info.Size(),
+		}
 
-        err := tw.WriteHeader(hdr)
+		err := tw.WriteHeader(hdr)
 
-        if err != nil {
-            log.Fatal(err)
-        }
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        file, err := os.Open(fr.Path)
+		file, err := os.Open(fr.Path)
 
-        if err != nil {
-            log.Fatal(err)
-        }
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        defer file.Close()
+		defer file.Close()
 
-        stat, err := file.Stat()
+		stat, err := file.Stat()
 
-        if err != nil {
-            log.Fatal(err)
-        }
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        bs := make([]byte, stat.Size())
-        _, err = file.Read(bs)
+		bs := make([]byte, stat.Size())
+		_, err = file.Read(bs)
 
-        if err != nil {
-            log.Fatal(err)
-        }
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        if _, err := tw.Write([]byte(bs)); err != nil {
-            log.Fatal(err)
-        }
+		if _, err := tw.Write([]byte(bs)); err != nil {
+			log.Fatal(err)
+		}
 
-    }
+	}
 
-    if err := tw.Close(); err != nil {
-        log.Fatal(err)
-    }
+	if err := tw.Close(); err != nil {
+		log.Fatal(err)
+	}
 
-    return f.Name()
+	return f.Name()
 
 }
 
 func encode(fp string, token bool) {
 
-    lmv_file := new(LMVFile)
+	lmv_file := new(LMVFile)
 
-    lmv_file.Algorithm = "SHA512"
+	lmv_file.Algorithm = "SHA512"
 
-    lmv_file.Name = filepath.Base(fp)
+	lmv_file.Name = filepath.Base(fp)
 
-    stat, err := os.Stat(fp)
+	stat, err := os.Stat(fp)
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    if stat.IsDir() {
-        fp = TarballDirectory(fp)
-        lmv_file.Tar = true
-        if v {
-            log.WithFields(logrus.Fields{
-                "directory": lmv_file.Name,
-            }).Info("Beginning new encode")
-        }
-    } else {
-        lmv_file.Tar = false
-        if v {
-            log.WithFields(logrus.Fields{
-                "file": lmv_file.Name,
-            }).Info("Beginning new encode")
-        }
-    }
+	if stat.IsDir() {
+		fp = TarballDirectory(fp)
+		lmv_file.Tar = true
+		if v {
+			log.WithFields(logrus.Fields{
+				"directory": lmv_file.Name,
+			}).Info("Beginning new encode")
+		}
+	} else {
+		lmv_file.Tar = false
+		if v {
+			log.WithFields(logrus.Fields{
+				"file": lmv_file.Name,
+			}).Info("Beginning new encode")
+		}
+	}
 
-    file, err := os.Open(fp)
+	file, err := os.Open(fp)
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    defer file.Close()
+	defer file.Close()
 
-    stat, err = file.Stat()
+	stat, err = file.Stat()
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    bs := make([]byte, stat.Size())
-    _, err = file.Read(bs)
+	bs := make([]byte, stat.Size())
+	_, err = file.Read(bs)
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    lmv_file.Size = stat.Size()
+	lmv_file.Size = stat.Size()
 
-    chunks := make([]LMVChunk, 1)
+	chunks := make([]LMVChunk, 1)
 
-    if stat.Size() <= CHUNK_SIZE {
+	if stat.Size() <= CHUNK_SIZE {
 
-        chunks[0] = LMVChunk {
-            CalculateSHA512(bs),
-            stat.Size(),
-            0,
-        }
+		chunks[0] = LMVChunk{
+			CalculateSHA512(bs),
+			stat.Size(),
+			0,
+		}
 
-        if v {
-            log.WithFields(logrus.Fields{
-                "count": 1,
-            }).Info("Breaking into chunks")
-        }
+		if v {
+			log.WithFields(logrus.Fields{
+				"count": 1,
+			}).Info("Breaking into chunks")
+		}
 
-    } else {
+	} else {
 
-        chunk_count := stat.Size() / CHUNK_SIZE + 1
+		chunk_count := stat.Size()/CHUNK_SIZE + 1
 
-        if v {
-            log.WithFields(logrus.Fields{
-                "count": chunk_count-1,
-            }).Info("Breaking into chunks")
-        }
+		if v {
+			log.WithFields(logrus.Fields{
+				"count": chunk_count - 1,
+			}).Info("Breaking into chunks")
+		}
 
-        chunks = make([]LMVChunk, chunk_count)
+		chunks = make([]LMVChunk, chunk_count)
 
-        for i := 0; i < len(chunks) - 1; i++ {
+		for i := 0; i < len(chunks)-1; i++ {
 
-            chunk := bs[int64(i)*CHUNK_SIZE:int64(i+1)*CHUNK_SIZE]
+			chunk := bs[int64(i)*CHUNK_SIZE : int64(i+1)*CHUNK_SIZE]
 
-            if v {
-                log.WithFields(logrus.Fields{
-                    "chunk#": i+1,
-                }).Info("Encoding chunk")
-            }
+			if v {
+				log.WithFields(logrus.Fields{
+					"chunk#": i + 1,
+				}).Info("Encoding chunk")
+			}
 
-            chunks[i] = LMVChunk{
-                CalculateSHA512(chunk),
-                CHUNK_SIZE,
-                i,
-            }
+			chunks[i] = LMVChunk{
+				CalculateSHA512(chunk),
+				CHUNK_SIZE,
+				i,
+			}
 
-        }
+		}
 
-        chunk := bs[int64(cap(chunks)-1)*CHUNK_SIZE:]
+		chunk := bs[int64(cap(chunks)-1)*CHUNK_SIZE:]
 
-        chunks[cap(chunks)-1] = LMVChunk{
-            CalculateSHA512(chunk),
-            int64(len(chunk)),
-            cap(chunks)-1,
-        }
+		chunks[cap(chunks)-1] = LMVChunk{
+			CalculateSHA512(chunk),
+			int64(len(chunk)),
+			cap(chunks) - 1,
+		}
 
-    }
+	}
 
-    lmv_file.Chunks = chunks
+	lmv_file.Chunks = chunks
 
-    if token {
+	if token {
 
-        upload_address := REGISTER + "/upload"
+		upload_address := REGISTER + "/upload"
 
-        packed, err := json.Marshal(lmv_file)
+		packed, err := json.Marshal(lmv_file)
 
-        if err != nil {
-            log.Fatal(err)
-        }
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        fields := make(url.Values)
-        fields.Set("file", string(packed))
+		fields := make(url.Values)
+		fields.Set("file", string(packed))
 
-        resp, err := http.PostForm(upload_address, fields)
+		resp, err := http.PostForm(upload_address, fields)
 
-        if err != nil {
-            log.Fatal(err)
-        }
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        defer resp.Body.Close()
+		defer resp.Body.Close()
 
-        body, err := ioutil.ReadAll(resp.Body)
+		body, err := ioutil.ReadAll(resp.Body)
 
-        if err != nil {
-            log.Fatal(err)
-        }
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        if v {
-            log.WithFields(logrus.Fields{
-                "token": string(body),
-            }).Info("Retrieved token from server")
-        }
+		if v {
+			log.WithFields(logrus.Fields{
+				"token": string(body),
+			}).Info("Retrieved token from server")
+		}
 
-        fmt.Println("'" + lmv_file.Name + "'" + " --> " + "'" + string(body) + "'")
+		fmt.Println("'" + lmv_file.Name + "'" + " --> " + "'" + string(body) + "'")
 
-    } else {
+	} else {
 
-        os.Create(lmv_file.Name + ".lmv")
+		os.Create(lmv_file.Name + ".lmv")
 
-        b, err := msgpack.Marshal(lmv_file)
+		b, err := msgpack.Marshal(lmv_file)
 
-        if err != nil {
-            log.Fatal(err)
-        }
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        err = ioutil.WriteFile(lmv_file.Name + ".lmv", b, 0644)
+		err = ioutil.WriteFile(lmv_file.Name+".lmv", b, 0644)
 
-        if err != nil {
-            log.Fatal(err)
-        }
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        if v {
-            log.WithFields(logrus.Fields{
-                "file": lmv_file.Name + ".lmv",
-            }).Info("Writing output to file")
-        }
+		if v {
+			log.WithFields(logrus.Fields{
+				"file": lmv_file.Name + ".lmv",
+			}).Info("Writing output to file")
+		}
 
-    }
+	}
 
 }
 
 func decode(input string, token bool) {
 
-    lmv_file := new(LMVFile)
+	lmv_file := new(LMVFile)
 
-    if token {
+	if token {
 
-        download_address := REGISTER + "/download/" + input
+		download_address := REGISTER + "/download/" + input
 
-        resp, err := http.Get(download_address)
+		resp, err := http.Get(download_address)
 
-        if err != nil {
-            log.Fatal(err)
-        }
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        defer resp.Body.Close()
+		defer resp.Body.Close()
 
-        body, err := ioutil.ReadAll(resp.Body)
+		body, err := ioutil.ReadAll(resp.Body)
 
-        if err != nil {
-            log.Fatal(err)
-        }
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        err = json.Unmarshal(body, &lmv_file)
+		err = json.Unmarshal(body, &lmv_file)
 
-        if err != nil {
-            log.Fatal(err)
-        }
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        if v {
-            log.WithFields(logrus.Fields{
-                "token": input,
-            }).Info("Retrieved data using token")
-        }
+		if v {
+			log.WithFields(logrus.Fields{
+				"token": input,
+			}).Info("Retrieved data using token")
+		}
 
-    } else {
+	} else {
 
-        file, err := os.Open(input)
+		file, err := os.Open(input)
 
-        if err != nil {
-            log.Fatal(err)
-        }
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        defer file.Close()
+		defer file.Close()
 
-        stat, err := file.Stat()
+		stat, err := file.Stat()
 
-        if err != nil {
-            log.Fatal(err)
-        }
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        bs := make([]byte, stat.Size())
-        _, err = file.Read(bs)
+		bs := make([]byte, stat.Size())
+		_, err = file.Read(bs)
 
-        if err != nil {
-            log.Fatal(err)
-        }
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        err = msgpack.Unmarshal(bs, lmv_file)
+		err = msgpack.Unmarshal(bs, lmv_file)
 
-        if err != nil {
-            log.Fatal(err)
-        }
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        if v {
-            log.WithFields(logrus.Fields{
-                "file": input,
-            }).Info("Unpacked .lmv file")
-        }
+		if v {
+			log.WithFields(logrus.Fields{
+				"file": input,
+			}).Info("Unpacked .lmv file")
+		}
 
-    }
+	}
 
-    bs := bytes.NewBuffer(make([]byte, 0))
+	bs := bytes.NewBuffer(make([]byte, 0))
 
-    for i := 0; i < len(lmv_file.Chunks); i++ {
+	for i := 0; i < len(lmv_file.Chunks); i++ {
 
-        chunk := make([]byte, lmv_file.Chunks[i].Size)
+		chunk := make([]byte, lmv_file.Chunks[i].Size)
 
-        f, err := os.Open("/dev/urandom")
+		f, err := os.Open("/dev/urandom")
 
-        for {
+		for {
 
-            _, err = f.Read(chunk)
+			_, err = f.Read(chunk)
 
-            if err != nil {
-                log.Fatal(err)
-            }
+			if err != nil {
+				log.Fatal(err)
+			}
 
-            if bytes.Equal([]byte(lmv_file.Chunks[i].Hash), []byte(CalculateSHA512(chunk))) {
-                break
-            }
+			if bytes.Equal([]byte(lmv_file.Chunks[i].Hash), []byte(CalculateSHA512(chunk))) {
+				break
+			}
 
-        }
+		}
 
-        bs.Write(chunk)
+		bs.Write(chunk)
 
-        if v {
-            log.WithFields(logrus.Fields{
-                "chunk#": i+1,
-            }).Info("Rebuilt chunk")
-        }
+		if v {
+			log.WithFields(logrus.Fields{
+				"chunk#": i + 1,
+			}).Info("Rebuilt chunk")
+		}
 
-    }
+	}
 
-    fo, err := os.Create(lmv_file.Name)
+	fo, err := os.Create(lmv_file.Name)
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    if _, err := fo.Write(bs.Bytes()); err != nil {
-        log.Fatal(err)
-    }
+	if _, err := fo.Write(bs.Bytes()); err != nil {
+		log.Fatal(err)
+	}
 
-    if v {
-        log.WithFields(logrus.Fields{
-            "file": lmv_file.Name,
-        }).Info("Writing output to file")
-    }
+	if v {
+		log.WithFields(logrus.Fields{
+			"file": lmv_file.Name,
+		}).Info("Writing output to file")
+	}
 
 }
 
 func init() {
-    log.Formatter = new(logrus.TextFormatter)
+	log.Formatter = new(logrus.TextFormatter)
 }
 
 func main() {
 
-    start := time.Now()
+	start := time.Now()
 
-    token := flag.Bool("token", false, "Use tokens in place of .lmv files")
-    verbose := flag.Bool("verbose", false, "Provide verbose output")
-    register := flag.String("register", "http://127.0.0.1:8081", "Register for tokens (including protocol)")
+	token := flag.Bool("token", false, "Use tokens in place of .lmv files")
+	verbose := flag.Bool("verbose", false, "Provide verbose output")
+	register := flag.String("register", "http://127.0.0.1:8081", "Register for tokens (including protocol)")
 
-    REGISTER = *register
+	REGISTER = *register
 
-    flag.Parse()
+	flag.Parse()
 
-    if *verbose {
-        v = true
-    } else {
-        v = false
-    }
+	if *verbose {
+		v = true
+	} else {
+		v = false
+	}
 
-    if v {
+	if v {
 
-        log.WithFields(logrus.Fields{
-            "token": *token,
-            "verbose": *verbose,
-            "register": *register,
-        }).Info("Parsed flags.")
+		log.WithFields(logrus.Fields{
+			"token":    *token,
+			"verbose":  *verbose,
+			"register": *register,
+		}).Info("Parsed flags.")
 
-    }
+	}
 
-    if len(os.Args) < 2 {
+	if len(os.Args) < 2 {
 
-        fmt.Println("Use lmv -h for usage")
+		fmt.Println("Use lmv -h for usage")
 
-    } else {
+	} else {
 
-        paths := strings.Split(os.Args[0], "/")
-        exec := paths[len(paths)-1]
+		paths := strings.Split(os.Args[0], "/")
+		exec := paths[len(paths)-1]
 
-        if exec == "lmv" {
+		if exec == "lmv" {
 
-            for i := 0; i < len(os.Args[1:]); i++ {
+			for i := 0; i < len(os.Args[1:]); i++ {
 
-                if _, err := os.Stat(os.Args[i+1]); err == nil {
+				if _, err := os.Stat(os.Args[i+1]); err == nil {
 
-                    encode(os.Args[i+1], *token)
+					encode(os.Args[i+1], *token)
 
-                }
+				}
 
-            }
+			}
 
-        } else if exec == "unlmv" {
+		} else if exec == "unlmv" {
 
-            for i := 0; i < len(os.Args[1:]); i++ {
+			for i := 0; i < len(os.Args[1:]); i++ {
 
-                if os.Args[i+1] != "--verbose" {
+				if os.Args[i+1] != "--verbose" {
 
-                    if _, err := os.Stat(os.Args[i+1]); err == nil {
+					if _, err := os.Stat(os.Args[i+1]); err == nil {
 
-                        decode(os.Args[i+1], false)
+						decode(os.Args[i+1], false)
 
-                    } else {
+					} else {
 
-                        decode(os.Args[i+1], true)
+						decode(os.Args[i+1], true)
 
-                    }
+					}
 
-                }
+				}
 
-            }
+			}
 
-        }
+		}
 
-    }
+	}
 
-    if v {
-        elapsed := time.Since(start)
+	if v {
+		elapsed := time.Since(start)
 
-        log.WithFields(logrus.Fields{
-            "time": string(elapsed),
-        }).Info("Completed execution.")
-    }
+		log.WithFields(logrus.Fields{
+			"time": string(elapsed),
+		}).Info("Completed execution.")
+	}
 
 }
--- register/server.go
+++ register/server.go
@@ -1,123 +1,123 @@
 package main
 
 import (
-    "crypto/rand"
-    "log"
+	"crypto/rand"
+	"log"
 
-    "labix.org/v2/mgo"
-    "labix.org/v2/mgo/bson"
-    "github.com/go-martini/martini"
-    "github.com/martini-contrib/render"
-    "github.com/martini-contrib/binding"
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/binding"
+	"github.com/martini-contrib/render"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
 )
 
 type LMVFile struct {
-    Size int64          `bson:"size"         json:"size"         binding:"required"`
-    Name string         `bson:"name"         json:"name"         binding:"required"`
-    Algorithm string    `bson:"algorithm"    json:"algorithm"    binding:"required"`
-    Chunks []LMVChunk   `bson:"chunks"       json:"chunks"       binding:"required"`
-    Tar bool            `bson:"tar"          json:"tar"`
-    Token string        `bson:"token"        json:"token"`
+	Size      int64      `bson:"size"         json:"size"         binding:"required"`
+	Name      string     `bson:"name"         json:"name"         binding:"required"`
+	Algorithm string     `bson:"algorithm"    json:"algorithm"    binding:"required"`
+	Chunks    []LMVChunk `bson:"chunks"       json:"chunks"       binding:"required"`
+	Tar       bool       `bson:"tar"          json:"tar"`
+	Token     string     `bson:"token"        json:"token"`
 }
 
 type LMVChunk struct {
-    Hash string         `bson:"hash"         json:"hash"         binding:"required"`
-    Size int64          `bson:"size"         json:"size"         binding:"required"`
-    Index int           `bson:"index"        json:"index"        binding:"required"`
+	Hash  string `bson:"hash"         json:"hash"         binding:"required"`
+	Size  int64  `bson:"size"         json:"size"         binding:"required"`
+	Index int    `bson:"index"        json:"index"        binding:"required"`
 }
 
 const (
-    token_length = 10
-    mgo_host = "localhost"
-    mgo_db = "Ludicrous-MV"
-    mgo_col = "Files"
+	token_length = 10
+	mgo_host     = "localhost"
+	mgo_db       = "Ludicrous-MV"
+	mgo_col      = "Files"
 )
 
-func randstr (length int) string {
+func randstr(length int) string {
 
-    const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    var bytes = make([]byte, length)
+	const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	var bytes = make([]byte, length)
 
-    rand.Read(bytes)
+	rand.Read(bytes)
 
-    for i, b := range bytes {
-        bytes[i] = alphanum[b % byte(len(alphanum))]
-    }
+	for i, b := range bytes {
+		bytes[i] = alphanum[b%byte(len(alphanum))]
+	}
 
-    return string(bytes)
+	return string(bytes)
 
 }
 
 func main() {
 
-    session, err := mgo.Dial(mgo_host)
+	session, err := mgo.Dial(mgo_host)
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    defer session.Close()
+	defer session.Close()
 
-    session.SetMode(mgo.Monotonic, true)
+	session.SetMode(mgo.Monotonic, true)
 
-    c := session.DB(mgo_db).C(mgo_col)
+	c := session.DB(mgo_db).C(mgo_col)
 
-    m := martini.Classic()
+	m := martini.Classic()
 	m.Use(render.Renderer())
 
 	m.Get("/file/:token", func(params martini.Params, r render.Render) {
 
-        n, err := c.Find(bson.M{"token": params["token"]}).Count()
+		n, err := c.Find(bson.M{"token": params["token"]}).Count()
 
-        if err != nil {
-            log.Fatal(err)
-        }
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        if n != 1 {
-            r.JSON(404, "")
-        } else {
-            var lmv_file LMVFile
+		if n != 1 {
+			r.JSON(404, "")
+		} else {
+			var lmv_file LMVFile
 
-            err = c.Find(bson.M{"token": params["token"]}).One(&lmv_file)
+			err = c.Find(bson.M{"token": params["token"]}).One(&lmv_file)
 
-            if err != nil {
-                log.Fatal(err)
-            }
+			if err != nil {
+				log.Fatal(err)
+			}
 
-            r.JSON(200, lmv_file)
-        }
+			r.JSON(200, lmv_file)
+		}
 
 	})
 
-    m.Post("/file", binding.Bind(LMVFile{}), func(params martini.Params, r render.Render, lmv_file LMVFile) {
+	m.Post("/file", binding.Bind(LMVFile{}), func(params martini.Params, r render.Render, lmv_file LMVFile) {
 
-        token := randstr(token_length)
+		token := randstr(token_length)
 
-        for {
-            n, err := c.Find(bson.M{"token": token}).Count()
+		for {
+			n, err := c.Find(bson.M{"token": token}).Count()
 
-            if err != nil {
-                log.Fatal(err)
-            }
+			if err != nil {
+				log.Fatal(err)
+			}
 
-            if n > 0 {
-                token = randstr(token_length)
-            } else {
-                break
-            }
-        }
+			if n > 0 {
+				token = randstr(token_length)
+			} else {
+				break
+			}
+		}
 
-        lmv_file.Token = token
+		lmv_file.Token = token
 
-        err := c.Insert(lmv_file)
+		err := c.Insert(lmv_file)
 
-        if err != nil {
-            log.Fatal(err)
-        }
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        r.JSON(200, "")
+		r.JSON(200, "")
 
-    })
+	})
 
 	m.Run()
 }
